4936bef180b3f8bf0e3c25738d1daccb386c4aef
Garrett Padera <pwnsauce@laveritas.com>
2015-11-25 22:40:56 -0800

    Gets all of a user's albums from Facebook. Right now the albums aren't being passed through the BroadcastReceiver because they aren't parcelable.

--- app/src/main/java/net/garrettsites/picturebook/commands/GetAllAlbumsService.java
+++ app/src/main/java/net/garrettsites/picturebook/commands/GetAllAlbumsService.java
@@ -15,7 +15,13 @@ import com.facebook.Profile;
 
 import net.garrettsites.picturebook.model.Album;
 
+import org.joda.time.DateTime;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Locale;
 
 /**
@@ -24,6 +30,8 @@ import java.util.Locale;
 public class GetAllAlbumsService extends IntentService {
 
     public static final String ARG_RECEIVER = "receiverTag";
+
+    private ArrayList<Album> allAlbums = new ArrayList<>();
 
     public GetAllAlbumsService() {
         super(GetAllAlbumsService.class.getName());
@@ -40,7 +48,7 @@ public class GetAllAlbumsService extends IntentService {
 
         Bundle parameters = new Bundle();
         parameters.putString("fields", "type,name,created_time,updated_time");
-        parameters.putString("limit", "999");
+        parameters.putString("limit", "50");
 
         GraphRequest request = new GraphRequest(
                 AccessToken.getCurrentAccessToken(),
@@ -48,11 +56,41 @@ public class GetAllAlbumsService extends IntentService {
                 parameters,
                 HttpMethod.GET);
 
-        GraphResponse response = request.executeAndWait();
-
-        // TODO: Convert GraphResponse into an ArrayList of Albums.
+        executeRequestAndAddAlbumsToList(request);
 
         receiver.send(Activity.RESULT_OK, new Bundle());
+    }
+
+    private void executeRequestAndAddAlbumsToList(GraphRequest request) {
+        GraphResponse response = request.executeAndWait();
+
+        try {
+            JSONArray albumListJson = response.getJSONObject().getJSONArray("data");
+            for (int i = 0; i < albumListJson.length(); i++) {
+                JSONObject thisAlbum = albumListJson.getJSONObject(i);
+
+                String type = thisAlbum.getString("type");
+                String name = thisAlbum.getString("name");
+                String createdTimeStr = thisAlbum.getString("created_time");
+                String updatedTimeStr = thisAlbum.getString("updated_time");
+                int id = thisAlbum.getInt("id");
+
+                DateTime createdTime = new DateTime(createdTimeStr);
+                DateTime updatedTime = new DateTime(updatedTimeStr);
+
+                Album album = new Album(type, name, createdTime, updatedTime, id);
+
+                allAlbums.add(album);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // If there are additional albums, grab them.
+        GraphRequest nextPage = response.getRequestForPagedResults(GraphResponse.PagingDirection.NEXT);
+        if (nextPage != null) {
+            executeRequestAndAddAlbumsToList(nextPage);
+        }
     }
 
     private ArrayList<Album> getAllAlbums() {
--- app/src/main/java/net/garrettsites/picturebook/model/Album.java
+++ app/src/main/java/net/garrettsites/picturebook/model/Album.java
@@ -1,7 +1,42 @@
 package net.garrettsites.picturebook.model;
 
+import org.joda.time.DateTime;
+
 /**
  * Created by Garrett on 11/20/2015.
  */
 public class Album {
+    private String mType;
+    private String mName;
+    private DateTime mCreatedTime;
+    private DateTime mUpdatedTime;
+    private int mId;
+
+    public Album(String type, String name, DateTime createdTime, DateTime updatedTime, int id) {
+        mType = type;
+        mName = name;
+        mCreatedTime = createdTime;
+        mUpdatedTime = updatedTime;
+        mId = id;
+    }
+
+    public String getType() {
+        return mType;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public DateTime getCreatedTime() {
+        return mCreatedTime;
+    }
+
+    public DateTime getUpdatedTime() {
+        return mUpdatedTime;
+    }
+
+    public int getId() {
+        return mId;
+    }
 }
