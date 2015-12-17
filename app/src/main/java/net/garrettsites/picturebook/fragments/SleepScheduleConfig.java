0be3f0da494f5461db1088eca093b9fa21a41ef6
Garrett Padera <pwnsauce@laveritas.com>
2015-12-17 08:49:04 -0800

    Put the sleep/wake time pickers in popup dialogs

--- app/src/main/java/net/garrettsites/picturebook/fragments/SleepScheduleConfig.java
+++ app/src/main/java/net/garrettsites/picturebook/fragments/SleepScheduleConfig.java
@@ -1,11 +1,14 @@
 package net.garrettsites.picturebook.fragments;
 
 import android.app.Fragment;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.ToggleButton;
 
@@ -14,13 +17,14 @@ import net.garrettsites.picturebook.R;
 import net.garrettsites.picturebook.model.UserPreferences;
 import net.garrettsites.picturebook.util.Wakeitizer;
 
-public class SleepScheduleConfig extends Fragment {
+public class SleepScheduleConfig extends Fragment implements View.OnClickListener {
 
     private static final String TAG = SleepScheduleConfig.class.getName();
 
-    private TimePicker mWakeTimePicker;
-    private TimePicker mSleepTimePicker;
     private ToggleButton mSleepWakeEnable;
+
+    private TextView mWakeTimeLabel;
+    private TextView mSleepTimeLabel;
 
     /**
      * Required empty public constructor.
@@ -31,26 +35,36 @@ public class SleepScheduleConfig extends Fragment {
     @Override
     public View onCreateView(final LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_sleep_schedule_config, container, false);
+        final View view = inflater.inflate(R.layout.fragment_sleep_schedule_config, container, false);
 
         // Get user preferences
         final UserPreferences userPreferences =
                 ((PicturebookApplication) getActivity().getApplication()).preferences;
 
-        mWakeTimePicker = (TimePicker) view.findViewById(R.id.wake_time_picker);
-        mSleepTimePicker = (TimePicker) view.findViewById(R.id.sleep_time_picker);
-        mSleepWakeEnable = (ToggleButton) view.findViewById(R.id.sleep_wake_enable);
-
         // Set UI controls to their values from config.
+        mSleepWakeEnable = (ToggleButton) view.findViewById(R.id.sleep_wake_enable);
         mSleepWakeEnable.setChecked(userPreferences.isSleeperWakerEnabled());
 
-        mWakeTimePicker.setCurrentHour(userPreferences.getWakeTimeHour());
-        mWakeTimePicker.setCurrentMinute(userPreferences.getWakeTimeMinute());
+        int wakeHour = userPreferences.getWakeTimeHour();
+        int wakeMinute = userPreferences.getWakeTimeMinute();
+        mWakeTimeLabel = ((TextView) view.findViewById(R.id.wake_time_label));
+        mWakeTimeLabel.setText(formatTime(wakeHour, wakeMinute));
 
-        mSleepTimePicker.setCurrentHour(userPreferences.getSleepTimeHour());
-        mSleepTimePicker.setCurrentMinute(userPreferences.getSleepTimeMinute());
+        int sleepHour = userPreferences.getSleepTimeHour();
+        int sleepMinute = userPreferences.getSleepTimeMinute();
+        mSleepTimeLabel = ((TextView) view.findViewById(R.id.sleep_time_label));
+        mSleepTimeLabel.setText(formatTime(sleepHour, sleepMinute));
+
+        if (mSleepWakeEnable.isChecked()) {
+            view.findViewById(R.id.edit_sleep_wake_time_container).setVisibility(View.VISIBLE);
+        } else {
+            view.findViewById(R.id.edit_sleep_wake_time_container).setVisibility(View.INVISIBLE);
+        }
 
         // Add listeners to the UI controls.
+        view.findViewById(R.id.edit_wake_time).setOnClickListener(this);
+        view.findViewById(R.id.edit_sleep_time).setOnClickListener(this);
+
         mSleepWakeEnable.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -58,40 +72,107 @@ public class SleepScheduleConfig extends Fragment {
                 Wakeitizer waker = Wakeitizer.getInstance(buttonView.getContext().getApplicationContext());
 
                 if (isChecked) {
-                    // Enable the sleeper & waker.
+                    // Enable the Waker.
                     int wakeHour = userPreferences.getWakeTimeHour();
                     int wakeMinute = userPreferences.getWakeTimeMinute();
 
                     waker.setDailyWakeTime(wakeHour, wakeMinute);
+
+                    // TODO: Enable the sleeper.
+
+                    view.findViewById(R.id.edit_sleep_wake_time_container).setVisibility(View.VISIBLE);
                 } else {
-                    // Disable the sleeper & waker.
                     waker.cancelWaker();
+
+                    // TODO: Disable the sleeper.
+
+                    view.findViewById(R.id.edit_sleep_wake_time_container).setVisibility(View.INVISIBLE);
                 }
-            }
-        });
-
-        // Update the app's alarm to wake the device at a given time each day.
-        mWakeTimePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
-            @Override
-            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
-                userPreferences.setWakeTime(hourOfDay, minute);
-
-                if (mSleepWakeEnable.isChecked()) {
-                    Wakeitizer waker =
-                            Wakeitizer.getInstance(inflater.getContext().getApplicationContext());
-                    waker.setDailyWakeTime(hourOfDay, minute);
-                }
-            }
-        });
-
-        // Update the app's alarm to sleep the device at at given time each day.
-        mSleepTimePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
-            @Override
-            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
-
             }
         });
 
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        final UserPreferences userPreferences =
+                ((PicturebookApplication) getActivity().getApplication()).preferences;
+
+        switch(v.getId()) {
+            case R.id.edit_wake_time:
+                int wakeHour = userPreferences.getWakeTimeHour();
+                int wakeMinute = userPreferences.getWakeTimeMinute();
+
+                new TimePickerDialog(getActivity(),
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                userPreferences.setWakeTime(hourOfDay, minute);
+
+                                if (mSleepWakeEnable.isChecked()) {
+                                    Wakeitizer waker =
+                                            Wakeitizer.getInstance(getActivity().getApplicationContext());
+                                    waker.setDailyWakeTime(hourOfDay, minute);
+                                }
+
+                                mWakeTimeLabel.setText(formatTime(hourOfDay, minute));
+                            }},
+                        wakeHour,
+                        wakeMinute,
+                        DateFormat.is24HourFormat(getActivity())).show();
+                break;
+
+            case R.id.edit_sleep_time:
+                int sleepHour = userPreferences.getSleepTimeHour();
+                int sleepMinute = userPreferences.getSleepTimeMinute();
+
+                new TimePickerDialog(getActivity(),
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                userPreferences.setSleepTime(hourOfDay, minute);
+
+                                if (!mSleepWakeEnable.isChecked()) {
+                                    // TODO: Schedule the sleeper to run at the specified time.
+                                }
+
+                                mSleepTimeLabel.setText(formatTime(hourOfDay, minute));
+                            }},
+                        sleepHour,
+                        sleepMinute,
+                        DateFormat.is24HourFormat(getActivity())).show();
+                break;
+        }
+    }
+
+    /**
+     * Formats a 24 hour time into a human readable 12 hour time string. ex) 22:30 -> 10:30 pm
+     * @param hour The hour [0-23]
+     * @param minute The minute [0-59]
+     * @return A formatted 12 hour time string in the format: hh:mm am/pm
+     */
+    private String formatTime(int hour, int minute) {
+        boolean am = true;
+
+        if (hour == 0) {
+            hour = 12;
+        } else if (hour > 12) {
+            am = false;
+            hour = hour - 12;
+        }
+
+        StringBuilder str = new StringBuilder();
+        str.append(hour);
+        str.append(":");
+        str.append(minute);
+        str.append(" ");
+
+        if (am)
+            str.append("am");
+        else
+            str.append("pm");
+
+        return str.toString();
     }
 }
