9554f5c5074e716e23638f788506ee0914982cf5
Garrett Padera <pwnsauce@laveritas.com>
2016-06-28 17:55:02 -0700

    Make Album an abstract class instead of an interface.

--- app/src/main/java/net/garrettsites/picturebook/model/Album.java
+++ app/src/main/java/net/garrettsites/picturebook/model/Album.java
@@ -1,5 +1,6 @@
 package net.garrettsites.picturebook.model;
 
+import android.os.Parcel;
 import android.os.Parcelable;
 
 import org.joda.time.DateTime;
@@ -9,40 +10,114 @@ import java.util.ArrayList;
 /**
  * Created by Garrett on 3/20/2016.
  */
-public interface Album extends Parcelable {
+public abstract class Album implements Parcelable {
+
+    protected String mName;
+    protected String mDescription;
+    protected DateTime mCreatedTime;
+    protected DateTime mUpdatedTime;
+    protected String mId;
+
+    protected ArrayList<Photo> mPhotos;
+
+    public Album(String name, String description, DateTime createdTime, DateTime updatedTime, String id) {
+        mName = name;
+        mDescription = description;
+        mCreatedTime = createdTime;
+        mUpdatedTime = updatedTime;
+        mId = id;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mName);
+        dest.writeString(mDescription);
+        dest.writeString(mCreatedTime.toString());
+        dest.writeString(mUpdatedTime.toString());
+        dest.writeString(mId);
+        dest.writeList(mPhotos);
+
+        doWriteToParcel(dest);
+    }
+
+    /**
+     * Offers any subclasses the opportunity to add their custom properties to the parcel. The base
+     * Album class's data is added to the parcel first.
+     * @param dest The parcel to write data to.
+     */
+    protected abstract void doWriteToParcel(Parcel dest);
+
+    protected Album(Parcel in) {
+        mName = in.readString();
+        mDescription = in.readString();
+        mCreatedTime = new DateTime(in.readString());
+        mUpdatedTime = new DateTime(in.readString());
+        mId = in.readString();
+
+        mPhotos = in.readArrayList(getClass().getClassLoader());
+
+        doReadFromParcel(in);
+    }
+
+    /**
+     * Offers subclasses the opportunity to read their custom properties from the parcel. The base
+     * Album class's data is read from the parcel first.
+     * @param in The parcel to read from.
+     */
+    protected abstract void doReadFromParcel(Parcel in);
+
     /**
      * @return The album's name.
      */
-    String getName();
+    public String getName() {
+        return mName;
+    }
 
     /**
      * @return A short description of the album.
      */
-    String getDescription();
+    public String getDescription() {
+        return mDescription;
+    }
 
     /**
      * @return The date the album was created.
      */
-    DateTime getCreatedTime();
+    public DateTime getCreatedTime() {
+        return mCreatedTime;
+    }
 
     /**
      * @return The date the album was last updated.
      */
-    DateTime getUpdatedTime();
+    public DateTime getUpdatedTime() {
+        return mUpdatedTime;
+    }
 
     /**
      * @return A unique ID referencing this album.
      */
-    String getId();
+    public String getId() {
+        return mId;
+    }
 
     /**
      * Sets this album's photos.
      * @param photos The photos to add to this album.
      */
-    void setPhotos(ArrayList<Photo> photos);
+    public void setPhotos(ArrayList<Photo> photos) {
+        mPhotos = photos;
+    }
 
     /**
      * @return The photos in this album.
      */
-    ArrayList<Photo> getPhotos();
+    public ArrayList<Photo> getPhotos() {
+        return mPhotos;
+    }
 }
